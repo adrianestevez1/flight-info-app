4829bc6a6edc1cc7bd6e49ed51a862e994ff6bf3
adrianestevez1 <adrianestevez2016@gmail.com>
2024-07-17 23:27:42 -0400

    feat: implementing app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+const redirectToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectToHome = () => redirectLoggedInTo(['home']);
+
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'signup',
+    pathMatch: 'full',
+    component: SignupComponent,
+    ...canActivate(redirectToHome)
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent,
+    ...canActivate(redirectToHome)
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent,
+    ...canActivate(redirectToLogin)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
